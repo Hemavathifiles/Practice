fdca8d3da4a389d966446ab4c15b4fcc7b2d23fd
hemav <hemav@LAPTOP-BH7SCHNS>
2024-07-12 16:04:17 +0530

    First commit

---
+++ Practice/src/demo/Variables.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class Variables {
+
+		//types of variables 1.local variable,instance variable and static variable
+			//instance variable
+			int a=10;	
+			//local variable
+		    void print() {
+		    	String msg="hello i am local variable";
+		    	System.out.println(msg);
+		    }
+			//static variable
+		    static String message="Hello i'm static variable";	
+			public static void main(String[] args) {
+				Variables ob=new Variables();	
+				System.out.println("The value of a"+ob.a);	
+				ob.print();	
+				System.out.println(message);	
+		       
+			}
+			
+		
+
+
+	}
+
+
