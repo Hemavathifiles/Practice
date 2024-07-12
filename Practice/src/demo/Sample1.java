659f4460151ffcc1292140d51c898a4cf6d44c81
hemav <hemav@LAPTOP-BH7SCHNS>
2024-07-12 16:25:05 +0530

    First commit

--- Practice/src/demo/Sample1.java
+++ Practice/src/demo/Sample1.java
@@ -1,10 +1,10 @@
 package demo;
 
-public class Sample {
+public class Sample1 {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 
-System.out.println("Welcome");
 	}
 
 }
